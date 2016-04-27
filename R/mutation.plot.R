868ab0c65e88ef0abe91abd215f4c5df77f1ca36
Grey Monroe <greymonroe@gmail.com>
2016-04-27 17:19:51 -0600

    added mutation.plot

---
+++ R/mutation.plot.R
@@ -0,0 +1,21 @@
+#' mutation.plot
+#'
+#' This function plots mutations along genemodels created with genemodel.plot
+#'
+#' @param start start position
+#' @param stop stop position
+#' @param text any text that you want displayed on the label
+#' @param drop how far below the gene model you want the mutation label to be placed
+#' @param col the color of the text and mutation line to be
+#' @param haplotypes the color of dots that you want to place along the mutation line to indicate some factor such as haplotype that the mutation belongs to
+#' @export
+#' @examples
+#' mutation.plot()
+#'
+
+mutation.plot<-function(start, stop, text, drop=-0.15, col, haplotypes=NULL)
+{
+  rect(start, .2, stop, drop+.01*length(haplotypes), col=col, border=col)
+  text( stop, drop, text, cex=0.7, col=col, pos=4, offset=0.1+.1*length(haplotypes))
+  for (i in 1:length(haplotypes)) points(stop, drop-0.05+(i-1)*0.1, col=haplotypes[i], pch=20, cex=1)
+}
